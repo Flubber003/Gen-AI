8b0188b6dc3efbcd17a136bcbe4081bcacdca341
Flubber003 <tjwilson003@gmail.com>
2025-05-12 15:19:16 -0400

    testing

---
+++ .idea/assign1.py
@@ -0,0 +1,10 @@
+name = "Trey"
+age = 22
+height = 5.0
+print("Hey there, my name is "+ name +"! I'm "+ str(age) +" years old and "+ str(height) +" feet tall.")
+num1 = 10
+num2 = 3
+print("The sum of 10 and 3 is", num1+num2)
+print("The difference of 10 and 3 is", num1-num2)
+print("The product of 10 and 3 is", num1*num2)
+print("The quotient of 10 and 3 is", num1/num2)
