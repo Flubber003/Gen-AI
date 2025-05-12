748552accfeecab86da3ccbbb97e3aee20c8483c
Flubber003 <tjwilson003@gmail.com>
2025-05-12 15:45:57 -0400

    testing

--- .idea/assign1.py
+++ .idea/assign1.py
@@ -8,3 +8,12 @@ print("The sum of 10 and 3 is", num1+num2)
 print("The difference of 10 and 3 is", num1-num2)
 print("The product of 10 and 3 is", num1*num2)
 print("The quotient of 10 and 3 is", num1/num2)
+print("Input a whole number here:")
+a = input()
+a = int(a)
+if(a>0):
+    print("This number is positive. Awesome!")
+elif(a<0):
+    print("This number is negative. Better luck next time!")
+else:
+    print("Zero it is. A perfect balance!")
