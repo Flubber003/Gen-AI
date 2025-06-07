b39c973935d8b74ec83fd3f2daa9b483fd10780c
Flubber003 <tjwilson003@gmail.com>
2025-06-07 17:08:41 -0400

    testing

---
+++ .idea/Strings/assign_1.py
@@ -0,0 +1,7 @@
+words = "Python is amazing!"
+one = words[0:6]
+two = words[10:17]
+three = words[::-1]
+print("First word: " + one)
+print("Amazing part: " + two)
+print("Reversed string: " + three)
