984bbceb5342b2a3d7e11ae61274c100c33f81ee
Flubber003 <tjwilson003@gmail.com>
2025-06-07 18:26:33 -0400

    testing

---
+++ .idea/Strings/assign_2.py
@@ -0,0 +1,9 @@
+base = " hello, python world! "
+base1 = base.strip()
+base2 = base1.capitalize()
+base3 = base2.replace("world","universe")
+base4 = base3.upper()
+print(base1)
+print(base2)
+print(base3)
+print(base4)
