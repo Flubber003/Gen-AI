a163e74deff7fa369e4026b55f4e0dfe108586e9
Flubber003 <tjwilson003@gmail.com>
2025-06-12 16:42:12 -0400

    testing

---
+++ .idea/Lists/task_1.py
@@ -0,0 +1,7 @@
+favfruit = ['grapes', 'watermelon', 'honeydew', 'cantaloupe', 'apple']
+print("Original list: " + str(favfruit))
+favfruit.append("strawberry")
+print("After adding a fruit: " + str(favfruit))
+favfruit.remove("apple")
+print("After removing a fruit: " + str(favfruit))
+print("Reversed list: " + str(favfruit[::-1]))
