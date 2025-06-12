93cf71b0b851ae0dc864bcbc53a9132ccf083fb0
Flubber003 <tjwilson003@gmail.com>
2025-06-12 19:46:26 -0400

    testing

---
+++ .idea/Functions/task_1.py
@@ -0,0 +1,6 @@
+def greet_user(name):
+    print("Hello,", name+"! Welcome aboard. ", end="")
+def add_numbers(a, b):
+    return a+b;
+greet_user("Alice")
+print("The sum of 3 and 5 is", add_numbers(3, 5), "\b.")
